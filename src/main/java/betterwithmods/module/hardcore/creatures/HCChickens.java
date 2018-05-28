928e475956f91b831a1203e770ba29badaa2b007
primetoxinz <primetoxinzz@gmail.com>
2018-05-27 22:02:48 -0400

    Incomplete work on HCChickens change

--- src/main/java/betterwithmods/module/hardcore/creatures/HCChickens.java
+++ src/main/java/betterwithmods/module/hardcore/creatures/HCChickens.java
@@ -5,9 +5,11 @@ import betterwithmods.common.BWMBlocks;
 import betterwithmods.common.entity.ai.AIFoodEggLayer;
 import betterwithmods.module.Feature;
 import betterwithmods.util.InvUtils;
+import betterwithmods.util.WorldUtils;
 import com.google.common.collect.Sets;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.init.Items;
@@ -18,6 +20,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -40,14 +43,14 @@ import java.util.Set;
  */
 public class HCChickens extends Feature {
 
-    public static Set<ItemStack> SEEDS;
-
     public static final ResourceLocation EGG_LAYER = new ResourceLocation(BWMod.MODID, "egglayer");
-    public static int timeToLayerEgg = 6000;
+    public static Set<ItemStack> SEEDS;
+    @SuppressWarnings("CanBeFinal")
+    @CapabilityInject(IEggLayer.class)
+    public static Capability<IEggLayer> EGG_LAYER_CAP = null;
 
     @Override
     public void setupConfig() {
-        timeToLayerEgg = loadPropInt("Time to lay egg", "Base ticks it takes for a feed chicken to lay an egg plus a random number of the same value", 6000);
     }
 
     @Override
@@ -57,15 +60,15 @@ public class HCChickens extends Feature {
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
-        CapabilityManager.INSTANCE.register(HCChickens.IEggLayer.class, new HCChickens.CapabilityEggLayer(), EggLayer.class);
+        CapabilityManager.INSTANCE.register(HCChickens.IEggLayer.class, new HCChickens.CapabilityEggLayer(), EggLayer::new);
     }
 
-	@Override
-	public void postInit(FMLPostInitializationEvent event) {
-		SEEDS = Sets.newHashSet(new ItemStack(BWMBlocks.HEMP), new ItemStack(Items.WHEAT_SEEDS), new ItemStack(Items.MELON_SEEDS), new ItemStack(Items.PUMPKIN_SEEDS), new ItemStack(Items.BEETROOT_SEEDS));
-	}
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+        SEEDS = Sets.newHashSet(new ItemStack(BWMBlocks.HEMP), new ItemStack(Items.WHEAT_SEEDS), new ItemStack(Items.MELON_SEEDS), new ItemStack(Items.PUMPKIN_SEEDS), new ItemStack(Items.BEETROOT_SEEDS));
+    }
 
-	@SubscribeEvent
+    @SubscribeEvent
     public void onAttachCap(AttachCapabilitiesEvent<Entity> event) {
         if (event.getObject() instanceof EntityChicken) {
             event.addCapability(EGG_LAYER, new ChickenProvider(new ItemStack(Items.EGG), SEEDS));
@@ -80,29 +83,28 @@ public class HCChickens extends Feature {
         }
     }
 
-
     @SubscribeEvent
     public void onEntityTick(LivingEvent.LivingUpdateEvent event) {
-        if (event.getEntityLiving().world.isRemote)
+        EntityLivingBase entityLiving = event.getEntityLiving();
+        if (entityLiving.world.isRemote)
             return;
-        if (event.getEntityLiving() instanceof EntityChicken) {
-            EntityChicken chicken = (EntityChicken) event.getEntityLiving();
+        if (entityLiving instanceof EntityChicken) {
+            EntityChicken chicken = (EntityChicken) entityLiving;
             //Stops vanilla egg dropping mechanic
             chicken.timeUntilNextEgg = 6000000;
         }
-        if (event.getEntityLiving().hasCapability(EGG_LAYER_CAP, EnumFacing.DOWN)) {
-            IEggLayer layer = event.getEntityLiving().getCapability(EGG_LAYER_CAP, EnumFacing.DOWN);
-            if (layer.canLayEgg()) {
-                event.getEntityLiving().playSound(SoundEvents.ENTITY_CHICKEN_EGG, 1.0F, (event.getEntityLiving().getRNG().nextFloat() - event.getEntityLiving().getRNG().nextFloat()) * 0.2F + 1.0F);
-                InvUtils.ejectStackWithOffset(event.getEntityLiving().world, event.getEntityLiving().getPosition(), layer.getEggItem());
-                layer.setFeed(false);
-            }
-            if (layer.isFeed()) {
-                layer.setTicks(layer.getTicks() - 1);
+        if (entityLiving.hasCapability(EGG_LAYER_CAP, EnumFacing.DOWN)) {
+            World world = entityLiving.getEntityWorld();
+            IEggLayer layer = entityLiving.getCapability(EGG_LAYER_CAP, EnumFacing.DOWN);
+            if (layer != null) {
+                if (layer.canLayEgg(world)) {
+                    entityLiving.playSound(SoundEvents.ENTITY_CHICKEN_EGG, 1.0F, (entityLiving.getRNG().nextFloat() - entityLiving.getRNG().nextFloat()) * 0.2F + 1.0F);
+                    InvUtils.ejectStackWithOffset(entityLiving.world, event.getEntityLiving().getPosition(), layer.getEggItem());
+                    layer.setFeed(false);
+                }
             }
         }
     }
-
 
     @SubscribeEvent
     public void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
@@ -122,15 +124,15 @@ public class HCChickens extends Feature {
 
     public interface IEggLayer {
 
-        boolean canLayEgg();
+        boolean canLayEgg(World world);
 
         boolean isFeed();
 
         void setFeed(boolean feed);
 
-        void setTicks(int tick);
+        int getLayTime();
 
-        int getTicks();
+        void setLayTime(int tick);
 
         ItemStack getEggItem();
 
@@ -144,7 +146,18 @@ public class HCChickens extends Feature {
             if (!isFeed()) {
                 if (isBreedingItem(stack)) {
                     setFeed(true);
-                    setTicks(timeToLayerEgg+entity.getRNG().nextInt(timeToLayerEgg));
+                    World world = entity.world;
+
+                    int nextDay;
+                    int day = WorldUtils.getCurrentDay(world);
+
+                    if (WorldUtils.isTimeFrame(entity.world, WorldUtils.TimeFrame.NIGHT)) {
+                        nextDay = (day + 2);
+                    } else {
+                        nextDay = (day + 1);
+                    }
+
+                    setLayTime((int) ((nextDay * WorldUtils.Time.DAY.getTicks()) + WorldUtils.TimeFrame.DAWN.randomBetween()));
                     stack.shrink(1);
                     entity.attackEntityFrom(new DamageSource("feed"), 0);
                 }
@@ -153,18 +166,13 @@ public class HCChickens extends Feature {
 
     }
 
-
-    @SuppressWarnings("CanBeFinal")
-    @CapabilityInject(IEggLayer.class)
-    public static Capability<IEggLayer> EGG_LAYER_CAP = null;
-
     public static class CapabilityEggLayer implements Capability.IStorage<IEggLayer> {
 
         @Nullable
         @Override
         public NBTBase writeNBT(Capability<IEggLayer> capability, IEggLayer instance, EnumFacing side) {
             NBTTagCompound tag = new NBTTagCompound();
-            tag.setInteger("ticks", instance.getTicks());
+            tag.setInteger("ticks", instance.getLayTime());
             tag.setBoolean("feed", instance.isFeed());
             return tag;
 
@@ -173,7 +181,7 @@ public class HCChickens extends Feature {
         @Override
         public void readNBT(Capability<IEggLayer> capability, IEggLayer instance, EnumFacing side, NBTBase nbt) {
             NBTTagCompound tag = (NBTTagCompound) nbt;
-            instance.setTicks(tag.getInteger("ticks"));
+            instance.setLayTime(tag.getInteger("ticks"));
             instance.setFeed(tag.getBoolean("feed"));
         }
     }
@@ -190,9 +198,12 @@ public class HCChickens extends Feature {
             this.feedItems = feedItems;
         }
 
+        public EggLayer() {
+        }
+
         @Override
-        public boolean canLayEgg() {
-            return ticks <= 0 && feed;
+        public boolean canLayEgg(World world) {
+            return isFeed() && getLayTime() <= world.getTotalWorldTime();
         }
 
         public boolean isFeed() {
@@ -204,13 +215,13 @@ public class HCChickens extends Feature {
         }
 
         @Override
-        public void setTicks(int tick) {
-            this.ticks = tick;
+        public int getLayTime() {
+            return this.ticks;
         }
 
         @Override
-        public int getTicks() {
-            return this.ticks;
+        public void setLayTime(int tick) {
+            this.ticks = tick;
         }
 
         @Override
--- src/main/java/betterwithmods/util/WorldUtils.java
+++ src/main/java/betterwithmods/util/WorldUtils.java
@@ -21,6 +21,7 @@ import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.StreamSupport;
 
@@ -46,7 +47,7 @@ public final class WorldUtils {
     }
 
     public static boolean isSolid(World world, BlockPos pos, EnumFacing facing, IBlockState state) {
-        return SOLID_MATERIALS.contains(state.getMaterial()) && state.getBlockFaceShape(world,pos,facing.getOpposite()) == BlockFaceShape.SOLID;
+        return SOLID_MATERIALS.contains(state.getMaterial()) && state.getBlockFaceShape(world, pos, facing.getOpposite()) == BlockFaceShape.SOLID;
     }
 
     /**
@@ -214,6 +215,10 @@ public final class WorldUtils {
         return date.get(Calendar.MONTH) == Calendar.APRIL && date.get(Calendar.DAY_OF_MONTH) == 1;
     }
 
+    public static int getCurrentDay(World world) {
+        return (int) (world.getTotalWorldTime() / Time.DAY.getTicks());
+    }
+
     public enum MoonPhase {
         Full,
         WaningGibbous,
@@ -225,6 +230,22 @@ public final class WorldUtils {
         WaxingGibbous
     }
 
+    public enum Time {
+        SECOND(0.27),
+        MINUTE(16.6),
+        HOUR(1000),
+        DAY(24000);
+        private double ticks;
+
+        Time(double ticks) {
+            this.ticks = ticks;
+        }
+
+        public double getTicks() {
+            return ticks;
+        }
+    }
+
     public enum TimeFrame {
         DAWN(0, 3600),
         NOON(5000, 7000),
@@ -232,7 +253,7 @@ public final class WorldUtils {
         MIDNIGHT(17000, 19000),
         NIGHT(13001, 24000),
         DAY(0, 13000);
-
+        private static final Random rand = new Random();
         private int start, end;
 
         TimeFrame(int start, int end) {
@@ -242,6 +263,10 @@ public final class WorldUtils {
 
         public boolean isBetween(int time) {
             return time >= start && time <= end;
+        }
+
+        public int randomBetween() {
+            return rand.nextInt((end - start) + 1) + start;
         }
     }
 
