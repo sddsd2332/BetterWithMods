b93873738580a5070b8b3ebe65ee165552107a8f
primetoxinz <primetoxinzz@gmail.com>
2018-05-28 21:41:32 -0400

    Another attempt to fix progress desync, by only send window properties when changed

--- src/main/java/betterwithmods/client/container/ContainerProgress.java
+++ src/main/java/betterwithmods/client/container/ContainerProgress.java
@@ -7,6 +7,8 @@ import net.minecraft.inventory.IContainerListener;
 public abstract class ContainerProgress extends Container {
     private IProgressSource progressSource;
 
+    private double previousProgress, previousMax;
+
     public ContainerProgress(IProgressSource progressSource) {
         this.progressSource = progressSource;
     }
@@ -14,6 +16,7 @@ public abstract class ContainerProgress extends Container {
     @Override
     public void addListener(IContainerListener listener) {
         super.addListener(listener);
+
         listener.sendWindowProperty(this, 0, progressSource.getProgress());
         listener.sendWindowProperty(this, 1, progressSource.getMax());
     }
@@ -22,14 +25,18 @@ public abstract class ContainerProgress extends Container {
     public void detectAndSendChanges() {
         super.detectAndSendChanges();
         for (IContainerListener craft : this.listeners) {
-            craft.sendWindowProperty(this, 0, progressSource.getMax());
-            craft.sendWindowProperty(this, 1, progressSource.getProgress());
+            if (this.previousProgress != progressSource.getProgress())
+                craft.sendWindowProperty(this, 0, progressSource.getMax());
+            if (this.previousMax != progressSource.getMax())
+                craft.sendWindowProperty(this, 1, progressSource.getProgress());
         }
+        this.previousProgress = progressSource.getProgress();
+        this.previousMax = progressSource.getMax();
     }
 
     @Override
     public void updateProgressBar(int id, int data) {
-        switch(id) {
+        switch (id) {
             case 0:
                 this.progressSource.setMax(data);
             case 1:
