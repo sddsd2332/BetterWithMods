028f517be3a6c14d74214279e42fbdd6c41abf87
primetoxinz <primetoxinzz@gmail.com>
2018-05-31 22:26:20 -0400

    Improve HCSpawn random point calculation.

---
+++ src/main/java/betterwithmods/module/hardcore/world/DebugCommand.java
@@ -0,0 +1,52 @@
+package betterwithmods.module.hardcore.world;
+
+import com.google.common.collect.Maps;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class DebugCommand extends CommandBase {
+    @Override
+    public String getName() {
+        return "debug";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return null;
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
+
+        new Thread(() -> {
+            try {
+                FileWriter writer = new FileWriter(new File("/home/tyler/hcspawn.csv"));
+                writer.write("pos,chunk\n");
+                World world = sender.getEntityWorld();
+                for (int i = 0; i < 2000; i++) {
+                    BlockPos pos = HCSpawn.getRandomPoint(world, world.getSpawnPoint(), 2000);
+                    ChunkPos c = world.getChunkFromBlockCoords(pos).getPos();
+                    writer.write(String.format("%d,%d\n",c.getXStart(), c.getZStart()));
+                }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+
+
+    }
+}
