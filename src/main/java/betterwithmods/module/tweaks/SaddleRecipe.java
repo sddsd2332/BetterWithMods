3a229dea2a8dc1f1ab0a7d9eae4ad7aea40bb9dc
primetoxinz <primetoxinzz@gmail.com>
2018-06-03 19:49:13 -0400

    Move jar libraries to a different folder so gradle doesn't take them without asking. Add glue recipe from saddles, closes #1010

--- src/main/java/betterwithmods/module/tweaks/SaddleRecipe.java
+++ src/main/java/betterwithmods/module/tweaks/SaddleRecipe.java
@@ -1,8 +1,20 @@
 package betterwithmods.module.tweaks;
 
+import betterwithmods.common.BWRegistry;
+import betterwithmods.common.items.ItemMaterial;
 import betterwithmods.module.Feature;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 public class SaddleRecipe extends Feature {
     public SaddleRecipe() {
+
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        BWRegistry.CAULDRON.addStokedRecipe(Ingredient.fromStacks(new ItemStack(Items.SADDLE)), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.GLUE, 2));
     }
 }
