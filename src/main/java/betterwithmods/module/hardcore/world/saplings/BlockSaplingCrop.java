047e5066557d058e411265ad5ba3ce202ee59ab4
primetoxinz <primetoxinzz@gmail.com>
2018-06-06 18:50:10 -0400

    Get rid of old BlockCrops hack around

--- src/main/java/betterwithmods/module/hardcore/world/saplings/BlockSaplingCrop.java
+++ src/main/java/betterwithmods/module/hardcore/world/saplings/BlockSaplingCrop.java
@@ -1,6 +1,5 @@
 package betterwithmods.module.hardcore.world.saplings;
 
-import betterwithmods.common.BWMRecipes;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.IGrowable;
@@ -8,9 +7,6 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -93,11 +89,6 @@ public class BlockSaplingCrop extends BlockBush implements IGrowable {
                 }
             }
         }
-    }
-    
-    @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return Items.AIR;
     }
 
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
