3c3171f91380da1542697a8f874ae04608cf8458
primetoxinz <primetoxinzz@gmail.com>
2018-07-04 23:25:38 -0400

    Fix dropping multiple waterwheels

--- src/main/java/betterwithmods/common/blocks/mechanical/BlockWaterwheel.java
+++ src/main/java/betterwithmods/common/blocks/mechanical/BlockWaterwheel.java
@@ -4,13 +4,10 @@ import betterwithmods.common.BWMItems;
 import betterwithmods.common.blocks.mechanical.tile.TileEntityWaterwheel;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import java.util.Random;
 
 public class BlockWaterwheel extends BlockAxleGenerator {
 
@@ -26,16 +23,6 @@ public class BlockWaterwheel extends BlockAxleGenerator {
     @Override
     public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
         super.onBlockAdded(world, pos, state);
-    }
-
-    @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return BWMItems.AXLE_GENERATOR;
-    }
-
-    @Override
-    public int damageDropped(IBlockState state) {
-        return 1;
     }
 
     @Override
