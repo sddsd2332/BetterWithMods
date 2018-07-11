6df0e6b668d4261af4f8ba790eda52eeaf9eaab8
primetoxinz <primetoxinzz@gmail.com>
2018-07-11 17:50:09 -0400

    Fix handcrank face shapes

--- src/main/java/betterwithmods/common/blocks/mechanical/BlockCrank.java
+++ src/main/java/betterwithmods/common/blocks/mechanical/BlockCrank.java
@@ -203,6 +203,6 @@ public class BlockCrank extends BWMBlock implements IMultiVariants, IOverpower {
 
     @Override
     public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
-        return face == EnumFacing.DOWN ? BlockFaceShape.UNDEFINED : super.getBlockFaceShape(worldIn, state, pos, face);
+        return face != EnumFacing.DOWN ? BlockFaceShape.UNDEFINED : super.getBlockFaceShape(worldIn, state, pos, face);
     }
 }
