2f18b53c4d0fbd1a609a14c657b1e34c0e8b69f9
primetoxinz <primetoxinzz@gmail.com>
2018-07-15 20:26:14 -0400

    Fix HCSaplings improperly handling modded saplings, fixes #1105

--- src/main/java/betterwithmods/module/hardcore/world/saplings/BlockSaplingCrop.java
+++ src/main/java/betterwithmods/module/hardcore/world/saplings/BlockSaplingCrop.java
@@ -1,5 +1,6 @@
 package betterwithmods.module.hardcore.world.saplings;
 
+import betterwithmods.common.BWMRecipes;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.IGrowable;
@@ -7,6 +8,8 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -162,4 +165,8 @@ public class BlockSaplingCrop extends BlockBush implements IGrowable {
     }
 
 
+    @Override
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        drops.add(BWMRecipes.getStackFromState(fullyGrown));
+    }
 }
