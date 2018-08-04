5b33a18540678f254900a51dce527f23981793ac
primetoxinz <primetoxinzz@gmail.com>
2018-08-04 19:20:22 -0400

    Fix dynamite exploding even when unlit
    (cherry picked from commit 1a17148)

--- src/main/java/betterwithmods/common/entity/EntityDynamite.java
+++ src/main/java/betterwithmods/common/entity/EntityDynamite.java
@@ -28,7 +28,7 @@ import net.minecraftforge.fluids.IFluidBlock;
 public class EntityDynamite extends Entity implements IProjectile {
     private static final float pi = 3.141593F;
     public ItemStack stack;
-    private int fuse;
+    private int fuse = -1;
 
     public EntityDynamite(World world) {
         this(world,0,0,0, new ItemStack(BWMItems.DYNAMITE));
@@ -41,7 +41,6 @@ public class EntityDynamite extends Entity implements IProjectile {
         this.stack = stack;
         this.fuse = 100;
         this.preventEntitySpawning = true;
-        this.stack = stack;
         this.isImmuneToFire = true;
     }
 
@@ -57,8 +56,7 @@ public class EntityDynamite extends Entity implements IProjectile {
         this.motionY = (-MathHelper.sin(this.rotationPitch / 180.0F * pi) * 0.4F);
         this.stack = stack;
         this.shoot(this.motionX, this.motionY, this.motionZ, 0.75F, 1.0F);
-        if (isLit)
-            fuse = 100;
+        this.fuse = isLit ? 100: -1;
     }
 
     @Override
