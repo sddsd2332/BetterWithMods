2278f04b33065728fe7fdee9d357be31b4354efb
michaelepps <mep053@gmail.com>
2018-08-08 21:32:38 -0400

    Actually check hellfire config

--- src/main/java/betterwithmods/module/hardcore/beacons/HellfireBeaconEffect.java
+++ src/main/java/betterwithmods/module/hardcore/beacons/HellfireBeaconEffect.java
@@ -22,6 +22,8 @@ public class HellfireBeaconEffect extends PotionBeaconEffect {
     @GameRegistry.ObjectHolder("minecraft:fire")
     public static final Block fire = null;
 
+    private static boolean catchFire = true;
+
     public HellfireBeaconEffect() {
         super("hellfire", new BlockIngredient("blockConcentratedHellfire"), EntityPlayer.class);
         this.addPotionEffect(MobEffects.FIRE_RESISTANCE, 120, PotionBeaconEffect.Amplification.LEVEL);
@@ -32,20 +34,22 @@ public class HellfireBeaconEffect extends PotionBeaconEffect {
     @Override
     public void setupConfig(String categoryName) {
         super.setupConfig(categoryName);
-        ConfigHelper.loadPropBool("catchFire", categoryName, "The beacon will catch fire when it applies it effect", true);
+        catchFire = ConfigHelper.loadPropBool("catchFire", categoryName, "The beacon will catch fire when it applies it effect", true);
     }
 
     @Override
     public void onBeaconCreate(@Nonnull World world, @Nonnull BlockPos pos, int beaconLevel) {
         BlockPos.MutableBlockPos firePos = new BlockPos.MutableBlockPos();
 
-        for (int range = 1; range <= beaconLevel; range++) {
-            for (int x = -range; x <= range; x++) {
-                for (int z = -range; z <= range; z++) {
-                    firePos.setPos(pos.getX() + x, pos.getY() - range + 1, pos.getZ() + z);
-                    IBlockState state = world.getBlockState(firePos);
-                    if(state.getBlock().isReplaceable(world, firePos)) {
-                        world.setBlockState(firePos, fire.getDefaultState());
+        if(catchFire) {
+            for (int range = 1; range <= beaconLevel; range++) {
+                for (int x = -range; x <= range; x++) {
+                    for (int z = -range; z <= range; z++) {
+                        firePos.setPos(pos.getX() + x, pos.getY() - range + 1, pos.getZ() + z);
+                        IBlockState state = world.getBlockState(firePos);
+                        if(state.getBlock().isReplaceable(world, firePos)) {
+                            world.setBlockState(firePos, fire.getDefaultState());
+                        }
                     }
                 }
             }
