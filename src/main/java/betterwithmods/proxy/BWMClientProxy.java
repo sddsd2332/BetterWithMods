bd6a5b9e5337d74ce9b496d8c89b9e2bb8d9030a
primetoxinz <primetoxinzz@gmail.com>
2018-09-30 15:43:08 -0400

    Fix onPostBaked

--- src/main/java/betterwithmods/proxy/BWMClientProxy.java
+++ src/main/java/betterwithmods/proxy/BWMClientProxy.java
@@ -1,12 +1,9 @@
 package betterwithmods.proxy;
 
-import betterwithmods.client.baking.BarkModel;
-import betterwithmods.client.model.render.RenderUtils;
 import betterwithmods.lib.ModLib;
 import betterwithmods.library.modularity.impl.proxy.ClientProxy;
 import net.minecraft.client.renderer.BannerTextures;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -15,8 +12,4 @@ public class BWMClientProxy extends ClientProxy {
     @SideOnly(Side.CLIENT)
     public static final BannerTextures.Cache WINDMILLS = new BannerTextures.Cache("betterwithmods:W", new ResourceLocation(ModLib.MODID, "textures/blocks/windmills/banner.png"), "betterwithmods:textures/blocks/windmills/");
 
-    @Override
-    public void onPostBake(ModelBakeEvent event) {
-
-    }
 }
