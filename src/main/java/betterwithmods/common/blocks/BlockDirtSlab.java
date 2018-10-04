9412862f4b0be995662d5ab37a340f6b39430989
primetoxinz <primetoxinzz@gmail.com>
2018-10-03 22:40:55 -0400

    Remove unused custom grass block

--- src/main/java/betterwithmods/common/blocks/BlockDirtSlab.java
+++ src/main/java/betterwithmods/common/blocks/BlockDirtSlab.java
@@ -1,9 +1,11 @@
 package betterwithmods.common.blocks;
 
+import betterwithmods.common.BWMBlocks;
 import betterwithmods.common.BWMItems;
 import betterwithmods.module.tweaks.GrassPath;
 import betterwithmods.util.WorldUtils;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirt;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
@@ -226,7 +228,7 @@ public class BlockDirtSlab extends BlockSimpleSlab {
     public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
         if (!worldIn.isRemote) {
             if (state.getValue(VARIANT) == DirtSlabType.GRASS) {
-                BlockGrassCustom.handleGrassSpreading(worldIn, pos, rand, getDefaultState().withProperty(VARIANT, DirtSlabType.DIRT));
+                handleGrassSpreading(worldIn, pos, rand, getDefaultState().withProperty(VARIANT, DirtSlabType.DIRT));
             } else if (state.getValue(VARIANT) == DirtSlabType.MYCELIUM) {
                 BlockMyceliumCustom.handleMyceliumSpreading(worldIn, pos, rand, getDefaultState().withProperty(VARIANT, DirtSlabType.DIRT));
             } else if (state.getValue(VARIANT) == DirtSlabType.DIRT)
@@ -304,4 +306,37 @@ public class BlockDirtSlab extends BlockSimpleSlab {
             return this.name;
         }
     }
+
+    public static void handleGrassSpreading(World worldIn, BlockPos pos, Random rand, IBlockState dirtState) {
+        if (!worldIn.isRemote) {
+            int shrinkLight = WorldUtils.getNaturalLightFromNeighbors(worldIn, pos.up());
+            int growthLight = shrinkLight - worldIn.getSkylightSubtracted();
+            if (shrinkLight < 9 && worldIn.getBlockState(pos.up()).getLightOpacity(worldIn, pos.up()) > 2) {
+                worldIn.setBlockState(pos, dirtState);
+            } else {
+                if (growthLight >= 11) {
+                    BlockPos blockpos = pos.add(rand.nextInt(3) - 1, rand.nextInt(5) - 3, rand.nextInt(3) - 1);
+
+                    if (blockpos.getY() >= 0 && blockpos.getY() < 256 && !worldIn.isBlockLoaded(blockpos)) {
+                        return;
+                    }
+
+                    IBlockState iblockstate = worldIn.getBlockState(blockpos.up());
+                    IBlockState iblockstate1 = worldIn.getBlockState(blockpos);
+
+                    if (iblockstate1.getBlock() == Blocks.DIRT && iblockstate1.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT && WorldUtils.getNaturalLightFromNeighbors(worldIn, blockpos.up()) >= 11 && iblockstate.getLightOpacity(worldIn, pos.up()) <= 2) {
+                        worldIn.setBlockState(blockpos, Blocks.GRASS.getDefaultState());
+                    } else if (iblockstate1.getBlock() == BWMBlocks.DIRT_SLAB && iblockstate1.getValue(BlockDirtSlab.VARIANT) == BlockDirtSlab.DirtSlabType.DIRT && WorldUtils.getNaturalLightFromNeighbors(worldIn, blockpos.up()) >= 11 && iblockstate.getLightOpacity(worldIn, pos.up()) <= 2) {
+                        worldIn.setBlockState(blockpos, BWMBlocks.DIRT_SLAB.getDefaultState().withProperty(BlockDirtSlab.VARIANT, BlockDirtSlab.DirtSlabType.GRASS));
+                    } else if (iblockstate1.getBlock() == Blocks.FARMLAND && rand.nextInt(3) == 0 && worldIn.isAirBlock(blockpos.up())) {
+                        int light = WorldUtils.getNaturalLightFromNeighbors(worldIn, blockpos.up());
+                        if (light >= 11) {
+                            worldIn.setBlockState(blockpos.up(), Blocks.TALLGRASS.getDefaultState());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
