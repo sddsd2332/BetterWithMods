c54319803418807e7a792ecddd11c7385fb07e47
primetoxinz <primetoxinzz@gmail.com>
2018-10-28 21:57:01 -0400

    Run after techborn, should fix #1249

--- src/main/java/betterwithmods/BWMod.java
+++ src/main/java/betterwithmods/BWMod.java
@@ -25,7 +25,7 @@ public class BWMod {
     public static final String MODID = "betterwithmods";
     public static final String VERSION = "%VERSION%";
     public static final String NAME = "Better With Mods";
-    public static final String DEPENDENCIES = "before:survivalist;after:traverse;after:thaumcraft;after:natura;after:mantle;after:tconstruct;after:minechem;after:natura;after:terrafirmacraft;after:immersiveengineering;after:mekanism;after:thermalexpansion;after:ctm;after:geolosys;after:pvj";
+    public static final String DEPENDENCIES = "before:survivalist;after:traverse;after:thaumcraft;after:natura;after:mantle;after:tconstruct;after:minechem;after:natura;after:terrafirmacraft;after:immersiveengineering;after:mekanism;after:thermalexpansion;after:ctm;after:geolosys;after:pvj;after:techreborn";
 
     public static Logger logger;
     @SuppressWarnings({"CanBeFinal", "unused"})
