535b851f3a23c2035db1bdee4f0a0a749a213bb0
primetoxinz <primetoxinzz@gmail.com>
2018-11-09 18:49:49 -0500

    Rethink detection of netherfortress: don't use the seed, actually iterate the structuremap and expand the structure bounding box temporarily to the closest chunk boundary, then caching the boolean.

--- src/main/java/betterwithmods/module/tweaks/NetherFortressSpawns.java
+++ src/main/java/betterwithmods/module/tweaks/NetherFortressSpawns.java
@@ -3,21 +3,33 @@ package betterwithmods.module.tweaks;
 import betterwithmods.lib.ModLib;
 import betterwithmods.library.common.modularity.impl.Feature;
 import com.google.common.collect.Lists;
+import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
+import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectIterator;
 import net.minecraft.entity.monster.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.ChunkGeneratorHell;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.structure.MapGenStructure;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraft.world.gen.structure.StructureStart;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.Iterator;
+
 @Mod.EventBusSubscriber(modid = ModLib.MODID)
 public class NetherFortressSpawns extends Feature {
+
+    private static final Object2BooleanMap<ChunkPos> NETHERFORTRESSES = new Object2BooleanOpenHashMap<>();
 
     @Override
     public String getDescription() {
@@ -43,15 +55,53 @@ public class NetherFortressSpawns extends Feature {
                 IChunkGenerator generator = ((ChunkProviderServer) provider).chunkGenerator;
                 if (generator instanceof ChunkGeneratorHell) {
                     ChunkGeneratorHell hell = (ChunkGeneratorHell) generator;
-                    Chunk chunk = world.getChunk(event.getPos());
-
-                    if (hell.genNetherBridge.canSpawnStructureAtCoords(chunk.x, chunk.z)) {
+                    if (hasNetherFortress(hell.genNetherBridge, event.getPos())) {
                         event.getList().clear();
                         event.getList().addAll(Lists.newArrayList(SPAWNS));
                     }
-
                 }
             }
         }
     }
+
+    public static boolean hasNetherFortress(MapGenStructure structure, BlockPos pos) {
+        ChunkPos chunkPos = new ChunkPos(pos);
+        if (NETHERFORTRESSES.containsKey(chunkPos))
+            return NETHERFORTRESSES.get(chunkPos);
+        boolean value = isStructureInChunk(structure, pos);
+        NETHERFORTRESSES.put(chunkPos, value);
+        return value;
+    }
+
+    public static StructureBoundingBox roundToChunk(StructureBoundingBox box) {
+        return new StructureBoundingBox((box.minX >> 4) * 16, 0, (box.minZ >> 4) * 16, (box.maxX >> 4) * 16, 128, (box.maxZ >> 4) * 16);
+    }
+
+    public static boolean isStructureInChunk(MapGenStructure structure, BlockPos pos) {
+        ObjectIterator objectiterator = structure.structureMap.values().iterator();
+        label31:
+
+        while (objectiterator.hasNext()) {
+            StructureStart structurestart = (StructureStart) objectiterator.next();
+            StructureBoundingBox box1 = roundToChunk(structurestart.getBoundingBox());
+            if (structurestart.isSizeableStructure() && box1.isVecInside(pos)) {
+                Iterator<StructureComponent> iterator = structurestart.getComponents().iterator();
+
+                while (true) {
+                    if (!iterator.hasNext()) {
+                        continue label31;
+                    }
+
+                    StructureComponent structurecomponent = iterator.next();
+
+                    if (roundToChunk(structurecomponent.getBoundingBox()).isVecInside(pos)) {
+                        break;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
