27acfbbb41748e9cd87c1a960287f05cc18d55d2
primetoxinz <primetoxinzz@gmail.com>
2018-11-09 17:11:50 -0500

    Fix crypts spawning in other dimensions and kinda fix cascading worldgen for them

--- src/main/java/betterwithmods/module/exploration/crypts/CryptGenerator.java
+++ src/main/java/betterwithmods/module/exploration/crypts/CryptGenerator.java
@@ -1,10 +1,10 @@
 package betterwithmods.module.exploration.crypts;
 
 import betterwithmods.lib.ModLib;
-import jdk.nashorn.internal.ir.Block;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -13,7 +13,6 @@ import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
-import java.util.Map;
 import java.util.Random;
 
 public class CryptGenerator implements IWorldGenerator {
@@ -24,12 +23,20 @@ public class CryptGenerator implements IWorldGenerator {
 
     private static final StoneProcessor stoneProcessor = new StoneProcessor();
 
+    public static final int CHUNK_SIZE = 16;
+
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-        final BlockPos basePos = new BlockPos(chunkX * 16 + random.nextInt(16), 100, chunkZ * 16 + random.nextInt(16));
+        if (world.provider.getDimensionType() != DimensionType.OVERWORLD)
+            return;
+
+        int x = (chunkX * CHUNK_SIZE) + 8 + random.nextInt(16);
+        int z = (chunkZ * CHUNK_SIZE) + 8 + random.nextInt(16);
+
+        final BlockPos basePos = new BlockPos(x, 100, z);
         placementSettings = new PlacementSettings().setRotation(Rotation.values()[random.nextInt(Rotation.values().length)]);
 
-        if(world instanceof WorldServer) {
+        if (world instanceof WorldServer) {
             WorldServer worldServer = (WorldServer) world;
             endRoomGenerator = new EndRoomGenerator(getTemplate(worldServer, "crypt_end"));
 
