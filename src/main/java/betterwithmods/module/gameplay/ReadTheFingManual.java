bb87daf07db0c878611a0c91803fae5dc4c3ca50
primetoxinz <primetoxinzz@gmail.com>
2018-11-10 20:53:52 -0500

    Work on integrating with Patchouli. To keep compatibility The RTFM book will disable itself if Patchouli is installed.

--- src/main/java/betterwithmods/module/gameplay/ReadTheFingManual.java
+++ src/main/java/betterwithmods/module/gameplay/ReadTheFingManual.java
@@ -7,14 +7,14 @@ import betterwithmods.util.InvUtils;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 import java.util.Locale;
 
 public class ReadTheFingManual extends Feature {
+
+
     @SubscribeEvent
     public void onCraftedEvent(PlayerEvent.ItemCraftedEvent event) {
         if (!event.player.world.isRemote && !event.crafting.isEmpty() && event.crafting.getItem() != BWMItems.MANUAL) {
