7bfa4b8ed0f7423b4c31aa4363e64d14ea745714
primetoxinz <primetoxinzz@gmail.com>
2018-11-18 22:22:15 -0500

    Begin works on custom patchouli components. Going to need to give recipes an identifier

---
+++ src/main/java/betterwithmods/module/compat/patchouli/ComponentBulk.java
@@ -0,0 +1,10 @@
+package betterwithmods.module.compat.patchouli;
+
+public class ComponentBulk extends ComponentText {
+
+
+    @Override
+    public void build(int componentX, int componentY, int pageNum) {
+        super.build(componentX, componentY, pageNum);
+    }
+}
---
+++ src/main/java/betterwithmods/module/compat/patchouli/ComponentText.java
@@ -0,0 +1,61 @@
+package betterwithmods.module.compat.patchouli;
+
+import com.google.gson.annotations.SerializedName;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.item.EnumDyeColor;
+import vazkii.patchouli.api.ICustomComponent;
+import vazkii.patchouli.api.VariableHolder;
+import vazkii.patchouli.client.book.gui.BookTextRenderer;
+import vazkii.patchouli.client.book.gui.GuiBook;
+
+public class ComponentText implements ICustomComponent {
+
+    protected int x, y;
+
+    @VariableHolder
+    @SerializedName("text")
+    public String text;
+
+    @VariableHolder
+    @SerializedName("color")
+    protected String colorStr;
+
+    @SerializedName("max_width")
+    protected int maxWidth = 116;
+    @SerializedName("line_height")
+    protected int lineHeight = 9;
+
+    private transient BookTextRenderer textRenderer;
+    private transient int color;
+
+    @Override
+    public void onDisplayed(GuiScreen parent) {
+        this.textRenderer = new BookTextRenderer((GuiBook) parent, this.text, this.x, this.y, this.maxWidth, this.lineHeight, this.color);
+    }
+
+    @Override
+    public void build(int componentX, int componentY, int pageNum) {
+        x = componentX;
+        y = componentY;
+
+        try {
+            this.color = Integer.parseInt(this.colorStr, 16);
+        } catch (NumberFormatException var5) {
+            this.color = EnumDyeColor.BLACK.getColorValue();
+        }
+    }
+
+    @Override
+    public void render(float pticks, int mouseX, int mouseY) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, 0);
+        textRenderer.render(mouseX, mouseY);
+        renderAdditional(pticks, mouseX, mouseY);
+        GlStateManager.popMatrix();
+    }
+
+    public void renderAdditional(float pticks, int mouseX, int mouseY) {
+
+    }
+}
