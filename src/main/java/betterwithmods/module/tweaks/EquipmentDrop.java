1e2a89981f71bf0644a477c63afdf654cdd72581
BordListian <bordlistian@hotmail.de>
2020-05-13 13:24:12 +0200

    - Rewrite EquipmentDrop tweak to make it properly drop on non-player kills.

--- src/main/java/betterwithmods/module/tweaks/EquipmentDrop.java
+++ src/main/java/betterwithmods/module/tweaks/EquipmentDrop.java
@@ -1,25 +1,76 @@
 package betterwithmods.module.tweaks;
 
+import betterwithmods.library.event.EntityDropEquipmentEvent;
 import betterwithmods.module.Feature;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
 
 /**
  * Created by primetoxinz on 4/20/17.
  */
 public class EquipmentDrop extends Feature {
 
-    @SubscribeEvent
+    /*@SubscribeEvent
     public void setDropChance(EntityJoinWorldEvent e) {
         if (e.getEntity() instanceof EntityLiving) {
             EntityLiving entity = (EntityLiving) e.getEntity();
             if (entity instanceof EntityZombie) {
                 for (EntityEquipmentSlot slot : EntityEquipmentSlot.values()) {
-                    entity.setDropChance(slot, 1);
+                    entity.setDropChance(slot, 1.1f);
                 }
+            }
+        }
+    }*/
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onEntityDrop(LivingDropsEvent e) {
+        if (e.getEntity() instanceof EntityLiving) {
+            EntityLiving entity = (EntityLiving) e.getEntity();
+            if (entity instanceof EntityZombie) {
+                for (EntityEquipmentSlot entityequipmentslot : EntityEquipmentSlot.values())
+                {
+                    boolean alreadyAdded = false;
+
+                    ItemStack itemstack = entity.getItemStackFromSlot(entityequipmentslot);
+                    for (EntityItem item : e.getDrops()) {
+                        if(item.getItem() == itemstack)
+                            alreadyAdded = true;
+                    }
+
+                    if(!alreadyAdded) {
+                        Random rand = entity.getRNG();
+                        if (!itemstack.isEmpty() && !EnchantmentHelper.hasVanishingCurse(itemstack)) {
+                            if (itemstack.isItemStackDamageable()) {
+                                itemstack.setItemDamage(itemstack.getMaxDamage() - rand.nextInt(1 + rand.nextInt(Math.max(itemstack.getMaxDamage() - 3, 1))));
+                            }
+
+                            EntityItem item = new EntityItem(entity.getEntityWorld(), entity.posX, entity.posY, entity.posZ, itemstack);
+                            item.setDefaultPickupDelay();
+                            e.getDrops().add(item);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onEquipmentDrop(EntityDropEquipmentEvent e) {
+        if (e.getEntity() instanceof EntityLiving) {
+            EntityLiving entity = (EntityLiving) e.getEntity();
+            if (entity instanceof EntityZombie) {
+                //e.setCanceled(true); //Reeeeee he didn't do it
             }
         }
     }
